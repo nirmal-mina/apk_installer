1379b2659906883accf2309361fcd89073baa784
nirmal01465 <cs1210588@iitd.ac.in>
2025-10-01 07:52:47 +0530

    chore(android): upgrade AGP and SDK versions + add namespace

--- test/apk_installer_test.dart
+++ test/apk_installer_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:apk_installer/apk_installer.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('apk_installer');
